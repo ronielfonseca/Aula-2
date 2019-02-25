0b890cfac0fceee2005fa625f5b8323eb3b23c7b
Roniel Fonseca <47761759+ronielfonseca@users.noreply.github.com>
2019-02-25 20:15:41 -0300

    Add files via upload

---
+++ Exemplo_1.c
@@ -0,0 +1,28 @@
+// Código em linguagem C
+#include <stdio.h>
+
+int main()
+{
+		
+	int A=4;
+	int B=4;
+	
+	if(A>B)
+	{
+		printf("A e maior que B! \n");
+	}
+	else
+	{
+		if(A<B)
+		{
+			printf("B e maior que A! \n");
+		}
+		else
+		{
+			printf("B e igual a A! \n");
+		}
+	}
+	
+	return 1;
+	
+}
---
+++ Exemplo_2.c
@@ -0,0 +1,19 @@
+//Código em linguagem C
+#include <stdio.h>
+// Programa 'Lê e imprime um valor'
+
+int main()
+{
+	system("cls");
+	int A=0;
+	
+	printf("Informe um valor A inteiro: \n");
+	
+	scanf("%i",&A);
+	
+	printf("O valor de A e:%i \n", A);
+	
+	system("pause");
+	
+	return 1;
+}
