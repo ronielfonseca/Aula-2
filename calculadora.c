e207d2c7705ab90c18a963e0f0b9aab823402217
Roniel Fonseca <47761759+ronielfonseca@users.noreply.github.com>
2019-02-25 21:39:02 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+main ()
+{
+  
+float n1,n2,resultado;
+char operador;
+
+
+printf("\n Digite o primeiro numero:");
+scanf ("%f",&n1);
+printf("\n Digite o segundo numero:");
+scanf ("%f",&n2);
+printf("\n Digite o operador:");
+scanf ("%s",&operador);
+
+getch();
+if (operador=='+') {
+resultado=n1+n2;    
+printf ("\n O resultado e: %f",resultado);
+
+
+}  
+  
+else
+if (operador=='-') {
+resultado=n1-n2;
+printf ("\n O resultado e: %f",resultado);
+
+}
+
+else
+if (operador=='/') {
+resultado=n1/n2;
+printf ("\n O resultado e: %f",resultado);
+
+}
+
+else  
+if (operador=='*') {
+resultado=n1*n2;
+printf ("\n O resultado e: %f",resultado);
+
+
+}
+getch();
+
+}
